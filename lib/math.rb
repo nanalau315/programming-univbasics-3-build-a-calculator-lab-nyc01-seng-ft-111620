7e1c715bb3895bf0b0ca1380455e649e89926029
Wing Nam Lau <nanalau315@hotmail.com>
2020-10-30 01:15:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  subtract = num1 - num2
+  subtract
 end
 
 def division(num1, num2)
-
+  divide = num2 / num1
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  modu = num2 % num1
+  modu
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
+  num
 end
